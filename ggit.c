6dc9ad04297c845082363edf86a668466a0998d5
Maxim Hrunenko <maxhrunenko@gmail.com>
2023-12-05 17:57:12 +0200

    test

---
+++ ggit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+int main (int argc, char **argv) {
+    if (argc < 2) {
+        fprintf(stderr, "ERROR: No args!");
+        return 0 ;
+    }
+    
+    system("git add .");
+
+    char *command = malloc(sizeof(char) * strlen(argv[1]) + 20);
+    system("git commit -m ");
+    strcpy(command, "git commit -m \"");
+    strcat(command, argv[1]);
+    strcat(command, "\"");
+    system(command);
+    system("git push origin master");
+}
